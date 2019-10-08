56aa5f5fad1f4e1644a4fb1574bc5a9eca3c4796
Vishal Singh <ivishalsinghofficial@gmail.com>
2019-10-08 15:56:11 +0530

    Final Release

---
+++ resources/js/queries.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+
+    /** Sticky Navigation  **/
+    $('#js--section-features').waypoint(function (direction) {
+        if (direction === 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '100px;'
+    });
+    
+    
+    
+    
+
+});
